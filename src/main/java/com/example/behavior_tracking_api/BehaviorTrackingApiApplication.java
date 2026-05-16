db2b8663c8561639ca03913711ad3aa83d1020fa
Zulker <nael.zulkar@gmail.com>
2026-05-16 07:46:10 +0200

    chore: initial spring boot setup with jpa and postgres

---
+++ src/main/java/com/example/behavior_tracking_api/BehaviorTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.behavior_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehaviorTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehaviorTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/behavior_tracking_api/BehaviorTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.behavior_tracking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehaviorTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
